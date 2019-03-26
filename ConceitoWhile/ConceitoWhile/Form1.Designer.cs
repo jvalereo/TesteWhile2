ee636ebc98d47578dd8483fe5455a4a764ca242c
Jonas Valereo <jvalereo2016@gmail.com>
2019-03-26 14:16:24 -0300

    Teste Comando While
    Projeto, programa básico,  comando While

---
+++ ConceitoWhile/ConceitoWhile/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ConceitoWhile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.BtnTeste = new System.Windows.Forms.Button();
+            this.BtnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumero.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(111, 70);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(55, 22);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "teste";
+            // 
+            // BtnTeste
+            // 
+            this.BtnTeste.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTeste.Location = new System.Drawing.Point(82, 154);
+            this.BtnTeste.Name = "BtnTeste";
+            this.BtnTeste.Size = new System.Drawing.Size(115, 37);
+            this.BtnTeste.TabIndex = 1;
+            this.BtnTeste.Text = "Teste While";
+            this.BtnTeste.UseVisualStyleBackColor = true;
+            this.BtnTeste.Click += new System.EventHandler(this.BtnTeste_Click);
+            // 
+            // BtnFechar
+            // 
+            this.BtnFechar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnFechar.Location = new System.Drawing.Point(178, 252);
+            this.BtnFechar.Name = "BtnFechar";
+            this.BtnFechar.Size = new System.Drawing.Size(90, 26);
+            this.BtnFechar.TabIndex = 2;
+            this.BtnFechar.Text = "Fechar";
+            this.BtnFechar.UseVisualStyleBackColor = true;
+            this.BtnFechar.Click += new System.EventHandler(this.BtnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(291, 300);
+            this.ControlBox = false;
+            this.Controls.Add(this.BtnFechar);
+            this.Controls.Add(this.BtnTeste);
+            this.Controls.Add(this.lblNumero);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aula Teste While";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Button BtnTeste;
+        private System.Windows.Forms.Button BtnFechar;
+    }
+}
+
